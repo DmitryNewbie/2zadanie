b7b60d06e5e46044ad21cc8f6e76a93ebcd5bc4e
Dmitry <zimzulka1@gmail.com>
2022-09-06 22:28:05 +0400

    Задание

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>99)
+{
+    Console.WriteLine("Третья цифра" + ((number%10)/1));
+}
+else
+{
+    Console.WriteLine("Третий цифры нет");
+}
---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите цифру обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void week  (int number)
+{
+    if(number==6 || number==7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else if(number<1 || number>7)
+    {
+        Console.WriteLine("Это не день");
+    }
+    else
+    {
+        Console.WriteLine("Это не выходной");
+    }
+}
+week(number);
